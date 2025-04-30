281a8aafa48711de512158df5c4e2d0fb44055b6
Jakub Tański <jakub.oskar.tanski@outlook.com>
2025-04-30 20:31:43 +0200

    added player collision

--- Overcooked/Assets/Scrips/Player.cs
+++ Overcooked/Assets/Scrips/Player.cs
@@ -12,9 +12,39 @@ public class Player : MonoBehaviour
     private void Update() {
         
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        
         Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        float moveDistance = Time.deltaTime * moveSpeed;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+
+        if (!canMove) {
+            
+            // Try movement only in X axis
+            
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, moveDistance);
+
+            if (canMove) {
+                moveDirection = moveDirectionX;
+            } 
+            
+            else {
+                // Try movement only in Z axis
+                
+                Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, moveDistance);
+                
+                if (canMove) {
+                    moveDirection = moveDirectionZ;
+                }
+            }
+        }
+        
+        if (canMove) {
+            transform.position += moveDirection * moveDistance;
+        }
         
         float rotationSpeed = 10f;
         if (moveDirection != Vector3.zero) {
