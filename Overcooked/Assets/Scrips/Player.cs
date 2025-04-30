5d27d8cbc6a1cc1f155f3f87e9e051981623817d
Jakub Tański <jakub.oskar.tanski@outlook.com>
2025-04-30 15:43:49 +0200

    added basic player visuals and animations

--- Overcooked/Assets/Scrips/Player.cs
+++ Overcooked/Assets/Scrips/Player.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    private bool isWalking;
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
 
@@ -20,10 +22,21 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.A)) {
             inputVector.x -= 1;
         }
-
+        
         inputVector = inputVector.normalized;
         Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
         
+        
+        float rotationSpeed = 10f;
+        if (moveDirection != Vector3.zero) {
+            transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
+        }
+
+        isWalking = moveDirection != Vector3.zero;
+    }
+
+    public bool IsWalking() {
+        return isWalking;
     }
 }
