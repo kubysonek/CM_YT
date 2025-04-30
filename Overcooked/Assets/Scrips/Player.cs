cc35cbc61df12ce18a60512d983c5fa131f49bb2
Jakub Tański <jakub.oskar.tanski@outlook.com>
2025-04-30 16:25:16 +0200

    added cinemachine and new input system

--- Overcooked/Assets/Scrips/Player.cs
+++ Overcooked/Assets/Scrips/Player.cs
@@ -6,27 +6,15 @@ using UnityEngine.EventSystems;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
     private bool isWalking;
+    
     private void Update() {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y += 1;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y -= 1;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x += 1;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x -= 1;
-        }
         
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        
         Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
-        
         
         float rotationSpeed = 10f;
         if (moveDirection != Vector3.zero) {
