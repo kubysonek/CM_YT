3eb5c9257d6d6b9a814ad8aa1e77a9184e201f97
Jakub Tański <jakub.oskar.tanski@outlook.com>
2025-05-06 17:51:22 +0200

    added Sound & Music Options

--- Overcooked/Assets/Scrips/UI/GamePauseUI.cs
+++ Overcooked/Assets/Scrips/UI/GamePauseUI.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GamePauseUI : MonoBehaviour {
+    
+    public static GamePauseUI Instance { get; private set; }
 
     [SerializeField] private Button resumeButton;
     [SerializeField] private Button mainMenuButton;
+    [SerializeField] private Button optionsButton;
 
     private void Awake() {
+        Instance = this;
+        
         resumeButton.onClick.AddListener(() => {
             GameManager.Instace.TogglePauseGame();
         });
         
         mainMenuButton.onClick.AddListener(() => {
             Loader.Load(Loader.Scene.MainMenuScene);
+        });
+        
+        optionsButton.onClick.AddListener(() => {
+            OptionsUI.Instance.Show();
+            Hide();
         });
     }
 
@@ -34,11 +44,11 @@ public class GamePauseUI : MonoBehaviour {
         Show();
     }
 
-    private void Show() {
+    public void Show() {
         gameObject.SetActive(true);
     }
     
-    private void Hide() {
+    public void Hide() {
         gameObject.SetActive(false);
     }
 }
